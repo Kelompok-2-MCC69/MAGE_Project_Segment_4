f80e780e3ba83dceb857eb849507cf9c09fb5ffc
unknown <arieasngariadep@gmail.com>
2022-10-17 01:02:29 +0700

    add backend and frontend to project

---
+++ Backend/src/test/java/Kelompok2/ProjectKPI/ProjectKpiApplicationTests.java
@@ -0,0 +1,13 @@
+package Kelompok2.ProjectKPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Frontend/src/main/java/Kelompok2/ProjectKPI/ProjectKpiApplication.java
@@ -0,0 +1,14 @@
+package Kelompok2.ProjectKPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKpiApplication.class, args);
+		System.out.println("Hello Buddy, Your App is Running");
+	}
+
+}
