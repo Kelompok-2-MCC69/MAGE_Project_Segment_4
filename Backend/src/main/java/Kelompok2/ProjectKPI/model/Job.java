cee007669ca4e25a40a3373ee5d46e95e464b960
unknown <arieasngariadep@gmail.com>
2022-10-20 04:31:58 +0700

    Fixing backend

--- Backend/src/main/java/Kelompok2/ProjectKPI/model/Job.java
+++ Backend/src/main/java/Kelompok2/ProjectKPI/model/Job.java
@@ -1,4 +1,26 @@
 package Kelompok2.ProjectKPI.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_job")
 public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String title;
 }
